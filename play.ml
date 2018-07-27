8a357fb5a6ecdfac854e882366d8ed9c20aad4cd
vagrant <vagrant@vagrant.vm>
2018-07-27 11:57:08 +0000

    implement search(tmp)

--- play.ml
+++ play.ml
@@ -2,8 +2,28 @@ open Array
 open Color 
 open Command 
 
-
 type board = color array array 
+
+let count board color : int = 
+  let s = ref 0 in 
+    for i=1 to 8 do 
+      for j=1 to 8 do
+        if board.(i).(j) = color then s := !s + 1 
+      done
+    done;
+    !s
+
+let print_board board = 
+  print_endline " |A B C D E F G H ";
+  print_endline "-+----------------";
+  for j=1 to 8 do 
+    print_int j; print_string "|";
+    for i=1 to 8 do 
+      print_color (board.(i).(j)); print_string " " 
+    done;
+    print_endline ""
+  done;
+  print_endline "  (X: Black,  O: White)"
 
 let init_board () = 
   let board = Array.make_matrix 10 10 none in 
@@ -21,6 +41,11 @@ let init_board () =
 
 let dirs = [ (-1,-1); (0,-1); (1,-1); (-1,0); (1,0); (-1,1); (0,1); (1,1) ]
 
+let rec get_list_max l i : (int * int) =
+  match l with
+  | []    -> (-100000000,-1)
+  | s::ts -> let p = get_list_max ts (i+1) in if s > (fst p) then (s, i) else p
+
 let flippable_indices_line board color (di,dj) (i,j) =
   let ocolor = opposite_color color in
   let rec f (di,dj) (i,j) r =
@@ -36,21 +61,16 @@ let flippable_indices_line board color (di,dj) (i,j) =
     else 
       [] in 
     f (di,dj) (i,j) []
-      
-    
 
 let flippable_indices board color (i,j) =
   let bs = List.map (fun (di,dj) -> flippable_indices_line board color (di,dj) (i+di,j+dj)) dirs in 
     List.concat bs 
     
-let is_effective board color (i,j) =
-  match flippable_indices board color (i,j) with 
-      [] -> false
-    | _  -> true 
+let flip_count board color (i,j) =
+  List.length (flippable_indices board color (i,j))
 
 let is_valid_move board color (i,j) =
-  (board.(i).(j) = none) && is_effective board color (i,j) 
-
+  (board.(i).(j) = none) && (flip_count board color (i,j) > 0)
 
 let doMove board com color =
   match com with 
@@ -65,45 +85,39 @@ let doMove board com color =
 
 let mix xs ys =
   List.concat (List.map (fun x -> List.map (fun y -> (x,y)) ys) xs)
-	     
 
 let valid_moves board color = 
   let ls = [1;2;3;4;5;6;7;8] in 
   List.filter (is_valid_move board color)
     (mix ls ls)
 
+let eval_board board color : int =
+  let value = 0 in 
+  let ocolor = opposite_color color in
+    (count board color) - (count board ocolor)
+
+let rec update_board board color depth (i,j) : int =
+  let ocolor = opposite_color color in
+  let flip_cells = flippable_indices board color (i,j) in
+  board.(i).(j) <- color;
+  List.iter (fun (ii,jj) -> (board.(ii).(jj) <- color);) flip_cells; 
+  let ret : int = if depth > 0 then -1*(fst (deep_search board ocolor (depth-1))) else eval_board board color in
+  List.iter (fun (ii,jj) -> (board.(ii).(jj) <- ocolor);) flip_cells; 
+  board.(i).(j) <- none; ret
+
+ and deep_search board color depth : (int * int) =
+  let ms = valid_moves board color in 
+  let vals = List.map (update_board board color depth) ms in
+  get_list_max vals 0
 
 let play board color = 
   let ms = valid_moves board color in 
     if ms = [] then 
       Pass 
     else 
-      let k = Random.int (List.length ms) in 
-      let (i,j) = List.nth ms k in 
+      let best = deep_search board color 3 in
+      let (i,j) = List.nth ms (snd best) in 
 	Mv (i,j) 
-
-let count board color = 
-  let s = ref 0 in 
-    for i=1 to 8 do 
-      for j=1 to 8 do
-        if board.(i).(j) = color then s := !s + 1 
-      done
-    done;
-    !s
-
-
-let print_board board = 
-  print_endline " |A B C D E F G H ";
-  print_endline "-+----------------";
-  for j=1 to 8 do 
-    print_int j; print_string "|";
-    for i=1 to 8 do 
-      print_color (board.(i).(j)); print_string " " 
-    done;
-    print_endline ""
-  done;
-  print_endline "  (X: Black,  O: White)"
-      
 
 let report_result board = 
   let _ = print_endline "========== Final Result ==========" in 
